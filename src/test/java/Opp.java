8bd03f516731ed11b34761961f7e6d0ea8a658b5
Dipput <dipalit26@gmail.com>
2025-12-31 11:08:02 +0530

    classes updated

--- src/test/java/Opp.java
+++ src/test/java/Opp.java
@@ -1,4 +1,11 @@
+import org.testng.annotations.Test;
 
 public class Opp {
 
+	@Test
+	
+	public void test2()
+	{
+		System.out.println("opp executed");
+	}
 }
--- src/test/java/OrgTest.java
+++ src/test/java/OrgTest.java
@@ -1,6 +1,7 @@
 
 public class OrgTest {
 public static void main(String[] args) {
+	
 	System.out.println("org updated");
 }
 }
